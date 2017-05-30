f23ebab261cce3797971987bf22a8dffb08459c0
Kyznec14 <pc_stalker_don@mail.ru>
2017-05-30 17:07:44 +0300

    data base sqlite

--- src/main/java/com/kyznec14/messenger/Server/Server.java
+++ src/main/java/com/kyznec14/messenger/Server/Server.java
@@ -1,7 +1,19 @@
 package com.kyznec14.messenger.Server;
+import java.sql.*;
 
+
+import java.net.ServerSocket;
 
 public class Server {
-
+    private ServerSocket server;
+    public static  void main(String [] args){
+        try{
+            class.forName("org.sqlite.JDBC");
+            Connection co=DriverManager.getConnection("jdbc:sqlite:items.sql");
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
 
